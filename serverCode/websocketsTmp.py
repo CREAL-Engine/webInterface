2ee3b8555bcf650e050496b16b68d482f11e1404
rascal1901 <snquick@ucdavis.edu>
2018-05-09 13:27:59 -0700

    Change server

--- serverCode/websocketsTmp.py
+++ serverCode/websocketsTmp.py
@@ -71,7 +71,8 @@ async def mainWsLoop(websocket, path):
 
 
 asyncio.get_event_loop().run_until_complete(
-	websockets.serve(echo, 'localhost', 8799))
+	#websockets.serve(echo, 'localhost', 8799))
+	websockets.serve(echo, '172.17.0.1', 8888))
 asyncio.get_event_loop().run_forever()
 
 
